145221315097c689b7b97bf5ecd5e8d9adbd18af
Mv0914 <151770649+Mv0914@users.noreply.github.com>
2024-06-27 18:12:31 -0400

    06/27 update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Automum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Automum.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.auto;
+package org.firstinspires.ftc.teamcode.Auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -6,39 +6,36 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Automum extends LinearOpMode {
-    DcMotor leftFront; //YOOOOOOOOOOOOOOOOOOOOOOOOOO//
+
+    DcMotor leftFront;
     DcMotor leftBack;
     DcMotor rightFront;
     DcMotor rightBack;
-    DcMotor leftLS;
-    DcMotor rightLS;
 
-    int leftLSPos;
-    int rightLSPos;
-
+    int leftFrontPOS;
+    int leftBackPOS;
+    int rightFrontPOS;
+    int rightBackPOS;
 
     final int TICKS_PER_INCH = 538;
 
-    Servo turn;
-     
+    @Override
+    public void runOpMode(){
 
-
-        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftFront = hardwareMap.get(DcMotor.class,"leftFront");
         leftBack = hardwareMap.get(DcMotor.class, "leftBack");
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
         rightBack = hardwareMap.get(DcMotor.class, "rightBack");
-        leftLS = hardwareMap.get(DcMotor.class, "leftLS");
-        rightLS = hardwareMap.get(DcMotor.class, "rightLS");
-
-        turn = hardwareMap.get(Servo.class,"turn");
 
         rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
         rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightLS.setDirection(DcMotorSimple.Direction.REVERSE);
 
         leftFront.setPower(0);
         leftBack.setPower(0);
         rightFront.setPower(0);
         rightBack.setPower(0);
-        turn.setPosition(0);
+
+    }
+
+    public void
 }
